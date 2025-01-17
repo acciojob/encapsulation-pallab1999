5633f69d492039ff57f28829f1c8a41f0c6748fb
pallab1999 <pallab.dutta1999@gmail.com>
2025-01-17 11:31:48 +0530

    solved encapsulation_Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        rw.setName("Pallab");
+//     'name' has private access in 'com. driver. RWOnly'
+//      System.out.println(rw.name);
+        System.out.println(rw.getName());
+
+    }
 }
